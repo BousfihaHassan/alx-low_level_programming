7e8b37c998cdbef6b2f98319be4c7088b8f0d66c
BousfihaHassan <Bousfihahassan2019@gmail.com>
2023-10-07 12:53:17 -0700

    a

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ *main:Entry
+ * 
+ * description: check if n varibale is zero or positive or negative
+ */
+
+int main()
+{
+	int n;
+	if( n == 0 )
+	{
+		printf("is zero");
+	}
+	else if (n >0 )
+	{
+		printf("is positive");
+	}
+	else
+	{
+		printf("is negative");
+	}
+}

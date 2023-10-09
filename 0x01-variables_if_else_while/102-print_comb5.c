aa8c74c4efc2d6eda4e4419fb465fa024190b9a3
BousfihaHassan <Bousfihahassan2019@gmail.com>
2023-10-09 13:30:10 -0700

    a

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,30 +10,29 @@
 
 int main(void)
 {
-	int i, j, k;
+	int i = 0;
+	int j = 1;
+	int k = 2;
 
-	i = 0;
 	while (i <= 7)
 	{
-		j = i + 1;
 		while (j <= 8)
 		{
-			k = j + 1;
 			while (k <= 9)
 			{
-				putchar((i % 10) + '0');
-				putchar((j % 10) + '0');
-				putchar((k % 10) + '0');
-				if (i != 7 || j != 8 || k != 9)
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
+				if (!(i == 7 && j == 8 && k == 9))
 				{
 					putchar(',');
 					putchar(' ');
 				}
 				k++;
 			}
-			j++;
+			k = ++j + 1;
 		}
-		i++;
+		j = ++i + 1;
 	}
 	putchar('\n');
 

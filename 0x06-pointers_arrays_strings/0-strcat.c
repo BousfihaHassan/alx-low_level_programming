d23fc9e0bd98e017397fe80ed80e2f702ac789cb
BousfihaHassan <Bousfihahassan2019@gmail.com>
2023-10-19 16:08:12 -0700

    a

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,13 +11,14 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int index = 0, dest_len = 0;
+	int c, c2;
 
-	while (dest[index++])
-		dest_len++;
+	c = 0;
 
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
+	while (dest[c])
+		c++;
+	for (c2 = 0; src[c2] ; c2++)
+		dest[c++] = src[c2];
 
 	return (dest);
 }
